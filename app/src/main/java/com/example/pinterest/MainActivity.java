0f1dff2c16ec758f229e5d19374c67a748318268
Rohitohlyan66 <ohlyanrohit66@gmail.com>
2020-10-12 20:11:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pinterest/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.pinterest;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomBar = findViewById(R.id.bottomBar);
+
+        bottomBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+
+                return true;
+            }
+        });
+    }
+}
